731c180517f329c9ef276d4ed2484190f1283347
ebbersjathomes <ebbersj@homes.com>
2014-03-16 15:56:54 -0400

    Added a simple express wrapper to the Map Reduce call

---
+++ appExpress.js
@@ -0,0 +1,36 @@
+var express	= require("express"),
+	Promise	= require("bluebird");
+
+var app = express();
+
+app.get("/serverStats.json",function(req, resp, next){
+	var fs		= Promise.promisifyAll(require("fs")),
+		date	= req.query.date || '20131113';
+	fs.readFileAsync("log/access_" + date)
+	.then(function(file){
+		return file.toString().split('\n');
+	})
+	.map(JSON.parse)
+	.reduce(function(report,thisEntry){
+		report.total++;
+		if(thisEntry.responseTime > 100){
+			report.slow++
+		}
+		return report;
+	},{"total" : 0, "slow" : 0})
+	.then(function(result){
+		resp.json(200,{"status" : true, "stats" : result});
+	})
+	.catch(function(e){
+		next(e);
+	})
+});
+
+app.use(function(err, req, resp, next){
+  console.error(err.stack);
+  resp.json(500, {"status" : false, "message" : "Hmm Something's not right"});
+});
+
+var server = app.listen(4545, function() {
+    console.log('Listening on port %d', server.address().port);
+});
