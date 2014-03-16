2f4086594322f31b617f827aa329387fb551e0a7
ebbersjathomes <ebbersj@homes.com>
2014-03-16 15:16:36 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var Promise = require("bluebird");
+var fs		= Promise.promisifyAll(require("fs")); //Adds a new method fooAsync in my case readFileAsync
+
+fs.readFileAsync("access_20131113")
+.then(function(file){
+	return file.toString().split('\n');
+})
+.map(JSON.parse)
+.reduce(function(report,thisEntry){
+	report.total++;
+	if(thisEntry.responseTime > 100){
+		report.slow++
+	}
+	return report;
+},{"total" : 0, "slow" : 0})
+.then(console.log)
+.catch(function(e){
+	console.error(e);
+})
